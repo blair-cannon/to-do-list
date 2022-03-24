e15a58f81d3c3c1a2b2605e8a000a38d47156a69
Blair Preston <blair.preston@student.awesomeinc.org>
2022-03-24 15:45:33 +0000

    checkbox and x are working for individual todo

--- src/components/Task.js
+++ src/components/Task.js
@@ -10,8 +10,6 @@ export default function Task({ todo, todos, setTodoList } ) {
         id: Date.now(), 
         status: 'active'
       })
-
-      console.log('rerender')
 // gets todo (description) from todo prop from input 
 // id and status are defined in useState 
 
@@ -22,12 +20,15 @@ export default function Task({ todo, todos, setTodoList } ) {
     })
   }
 
-  function handleDelete(e) {
-    console.log('deleted');
-    const updatedTodos = todos.filter(todo => todo !== todo.id);
-    setTodoList(updatedTodos);
-    // not rerendering or not connecting back to todolist or logic is off ? 
+  function handleDeleteTodo(e) {
+    setTodoList((todos) => {
+      let targetTodo = todos.find(e => e.id === todo.id)
+      let updatedTodos = todos.filter(todo => todo !== targetTodo);
+      return updatedTodos;
+    })
   }
+  // find clicked target todo
+  // filter for array to not include the target todo
 
   return (
     <div>
@@ -35,7 +36,7 @@ export default function Task({ todo, todos, setTodoList } ) {
         <ListGroup.Item>  
             <input onChange={handleCheckClick} className="marginRight" type="checkbox" />
             <label className="marginRight">{todo}</label>
-          <CloseButton onClick={handleDelete} />
+          <CloseButton onClick={handleDeleteTodo}  />
         </ListGroup.Item>
       </ListGroup>
     
